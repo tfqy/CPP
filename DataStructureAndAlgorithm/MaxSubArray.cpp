0fae4f4ca451fb1d9f59a45d8240650e76784d05
tfqy <1115142237@qq.com>
2023-11-20 08:39:00 +0800

    每日一题

---
+++ DataStructureAndAlgorithm/MaxSubArray.cpp
@@ -0,0 +1,27 @@
+#include "algorithm"
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class Solution
+{
+public:
+  int maxSubArray(vector<int> &nums)
+  {
+    int pre = 0, maxAns = nums[0];
+    for (const auto &x : nums)
+    {
+      pre = max(pre + x, x);
+      maxAns = max(maxAns, pre);
+    }
+    return maxAns;
+  }
+};
+
+int main(int argc, char const *argv[])
+{
+  Solution *obj = new Solution();
+  vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  cout << obj->maxSubArray(nums) << endl;
+  return 0;
+}
