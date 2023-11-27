c0897f00d5fb633d066b8d15e2bd4372949733fb
tfqy <1115142237@qq.com>
2023-11-28 07:53:50 +0800

    每日一题

---
+++ DataStructureAndAlgorithm/FrontMiddleBackQueue.cpp
@@ -0,0 +1,83 @@
+#include "deque"
+using namespace std;
+
+class FrontMiddleBackQueue
+{
+
+private:
+  deque<int> q;
+
+public:
+  FrontMiddleBackQueue()
+  {
+    q.clear();
+  }
+
+  void pushFront(int val)
+  {
+    q.push_front(val);
+  }
+
+  void pushMiddle(int val)
+  {
+    q.insert(q.begin() + q.size() / 2, val);
+  }
+
+  void pushBack(int val)
+  {
+    q.push_back(val);
+  }
+
+  int popFront()
+  {
+    if (q.empty())
+    {
+      return -1;
+    }
+    int ans = q.front();
+    q.pop_front();
+    return ans;
+  }
+
+  int popMiddle()
+  {
+    if (q.empty())
+    {
+      return -1;
+    }
+    if (q.size() % 2 == 0)
+    {
+      int ans = q[q.size() / 2 - 1];
+      q.erase(q.begin() + q.size() / 2 - 1);
+      return ans;
+    }
+    else
+    {
+      int ans = q[q.size() / 2];
+      q.erase(q.begin() + q.size() / 2);
+      return ans;
+    }
+  }
+
+  int popBack()
+  {
+    if (q.empty())
+    {
+      return -1;
+    }
+    int ans = q.back();
+    q.pop_back();
+    return ans;
+  }
+};
+
+/**
+ * Your FrontMiddleBackQueue object will be instantiated and called as such:
+ * FrontMiddleBackQueue* obj = new FrontMiddleBackQueue();
+ * obj->pushFront(val);
+ * obj->pushMiddle(val);
+ * obj->pushBack(val);
+ * int param_4 = obj->popFront();
+ * int param_5 = obj->popMiddle();
+ * int param_6 = obj->popBack();
+ */
---
+++ DataStructureAndAlgorithm/MinDeletion.cpp
@@ -0,0 +1,38 @@
+#include "algorithm"
+#include "iostream"
+#include "vector"
+using namespace std;
+
+class Solution
+{
+public:
+  int minDeletion(vector<int> &nums)
+  {
+    int n, len, p1 = 0, p2 = 1, ans = 0;
+    n = len = nums.size();
+    if (!len)
+    {
+      return 0;
+    }
+    if (len == 1)
+    {
+      return 1;
+    }
+    while (p1 < n)
+    {
+      while (p2 < len && nums[p1] == nums[p2])
+      {
+        p2++;
+        ans++;
+        n--;
+      }
+      p1 = p2 + 1;
+      p2 = p1 + 1;
+    }
+    if (n & 1)
+    {
+      return ans + 1;
+    }
+    return ans;
+  }
+};
